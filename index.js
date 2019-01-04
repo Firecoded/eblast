aabe3f81a762721e8f15d64291c836cb489da25c
firecoded <yepyepguru@gmail.com>
2019-01-03 17:05:03 -0800

    survey schema configured, added credits check middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/user');
+require('./models/survey');
 require('./services/passport');
 
 
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
